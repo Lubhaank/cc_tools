10d4ab7d4abe1efbf82267e2f1f64d532c5343f3
Lubhaank <lubhaankumar@gmail.com>
2019-02-19 18:04:02 -0500

    first handin

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -5,3 +5,11 @@ input_dat_file = "data/pfgd_test.dat"
 
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+f= open("data/pfgd_test.txt","w+")
+
+
+
+print(cc_dat_utils.make_cc_data_from_dat(input_dat_file), file = f)
+
+#f.close()
